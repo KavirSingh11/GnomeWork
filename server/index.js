64e5c5938de311edea6e53a463b72e22bcee1a36
KavirSingh11 <kavir.singh11@gmail.com>
2020-09-03 13:56:25 -0400

    initial server setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const keys = require("./config/keys");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+mongoose.connect(
+	process.env.MONGODB_URI || keys.MongoURI,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	() => {
+		console.log("Database connected...");
+	}
+);
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log(`Server up on port ${PORT}...`);
+});
